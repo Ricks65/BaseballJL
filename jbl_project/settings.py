50248cdeba1f5496af4f2952bada62968ad67170
Ricks65 <kr0192@outlook.es>
2026-07-14 12:58:15 -0600

    Descripción

--- jbl_project/settings.py
+++ jbl_project/settings.py
@@ -80,16 +80,14 @@ WSGI_APPLICATION = 'jbl_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'new_jbl',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
