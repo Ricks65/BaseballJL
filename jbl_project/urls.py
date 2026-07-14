d1c8d1d2a8cd59cab7a15a48b88a377d481020f8
Ricks65 <kr0192@outlook.es>
2026-07-14 12:52:39 -0600

    Descripción

--- jbl_project/urls.py
+++ jbl_project/urls.py
@@ -4,9 +4,14 @@ URL configuration for jbl_project project.
 See: https://docs.djangoproject.com/en/6.0/topics/http/urls/
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('/league/')
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('league/', include('apps.league.urls')),
 ]
