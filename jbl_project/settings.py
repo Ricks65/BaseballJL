5c6b33424a078df48f995955940236c504aa40a0
Ricks65 <kr0192@outlook.es>
2026-07-16 19:51:00 +0000

    Configurar base de datos de Render

--- jbl_project/settings.py
+++ jbl_project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -161,9 +163,6 @@ WSGI_APPLICATION = 'jbl_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-import os
-import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
